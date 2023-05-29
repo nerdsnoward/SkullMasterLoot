1f9447d31342e98b2387ab216d34a352a7a1e5c9
Charles <samannaphala@gmail.com>
2023-05-29 17:57:58 +0300

    moves assignment inside if block

--- SkullMasterLoot.lua
+++ SkullMasterLoot.lua
@@ -2,11 +2,11 @@ local EventFrame = CreateFrame("Frame")
 EventFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
 EventFrame:SetScript("OnEvent", function(self, event, ...)
     local IsFightingBoss = UnitLevel("target") < 0 or UnitLevel("targettarget") < 0
-    local BossName = (UnitLevel("target") < 0 and UnitName("target")) or (UnitLevel("targettarget") < 0 and UnitName("targettarget"))
 
     if IsFightingBoss and GetLootMethod() ~= "master" and GetNumRaidMembers() > 0 and IsRaidLeader() then
-        ChatFrame1:AddMessage('SkullMasterLoot detected a boss! Switching to master loot.')
+        local BossName = (UnitLevel("target") < 0 and UnitName("target")) or (UnitLevel("targettarget") < 0 and UnitName("targettarget"))
 
+        ChatFrame1:AddMessage('SkullMasterLoot detected a boss! Switching to master loot.')
         SetLootMethod("master", UnitName("player"))
         SendChatMessage(UnitName("player") .. " " .. "switched to master loot for" .. " " .. BossName,
             "RAID_WARNING")
